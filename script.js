be4a61a8f4b8b3ca10d7b969deba525789dbad2c
oharomari <128266847+oharomari@users.noreply.github.com>
2023-03-18 22:51:13 -0300

    scielo

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
+// URL do CORS Anywhere
+const proxyUrl = 'https://cors-anywhere-for-everyone.herokuapp.com/';
+
 // URL da API do PubMed
-const url = 'https://cors-anywhere-for-everyone.herokuapp.com/https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=(echocardiography[Title/Abstract])+AND+(preterm[Title/Abstract])&retmode=json&retmax=20&sort=relevance&field=pubdate';
+const pubmedUrl = `${proxyUrl}https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=(echocardiography[Title/Abstract])+AND+(preterm[Title/Abstract])&retmode=json&retmax=20&sort=relevance&field=pubdate`;
+
+// URL da API do Google Scholar
+const googleUrl = `${proxyUrl}https://api.scraperapi.com/?api_key=<SUA_API_KEY>&url=https://scholar.google.com/scholar?q=echocardiography+preterm&hl=en&as_sdt=0,5&sciodt=0,5&cites=0&sort=relevance&num=20`;
+
+// URL da API do Scielo
+const scieloUrl = `${proxyUrl}https://api.scraperapi.com/?api_key=<SUA_API_KEY>&url=https://search.scielo.org/?q=echocardiography+preterm&lang=pt&count=20`;
 
 // Função para buscar artigos na API do PubMed
-async function buscarArtigos() {
+async function buscarArtigosPubmed() {
   try {
     // Fazer a solicitação de busca
-    const response = await axios.get(url);
-    const data = response.data;
+    const response = await fetch(pubmedUrl, {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    });
+    const data = await response.json();
 
     // Obter lista de IDs de artigos
     const ids = data.esearchresult.idlist;
 
     // Para cada ID, obter informações do artigo
     const artigos = await Promise.all(ids.map(async (id) => {
-      const res = await axios.get(`https://cors-anywhere-for-everyone.herokuapp.com/https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=${id}&retmode=json`);
-      const json = res.data;
+      const res = await fetch(`${proxyUrl}https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=${id}&retmode=json`, {
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      });
+      const json = await res.json();
       return json.result[id];
     }));
 
-    // Ordenar artigos por data de publicação
-    artigos.sort((a, b) => new Date(b.pubdate) - new Date(a.pubdate));
-
-    // Exibir os artigos na página
-    const listaArtigos = document.getElementById('lista-artigos');
-    listaArtigos.innerHTML = '';
-    artigos.forEach((artigo) => {
-      const li = document.createElement('li');
-      const doi = artigo.articleids.filter
+   
