d8db07a1f4ab8bcc74aecb4ba39e91f7b368d7af
oharomari <128266847+oharomari@users.noreply.github.com>
2023-03-18 22:46:06 -0300

    proxyh again

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-// URL do CORS Anywhere
-const proxyUrl = 'https://cors-anywhere-for-everyone.herokuapp.com/';
-
 // URL da API do PubMed
-const url = `${proxyUrl}https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=(echocardiography[Title/Abstract])+AND+(preterm[Title/Abstract])&retmode=json&retmax=20&sort=relevance&field=pubdate`;
+const url = 'https://cors-anywhere-for-everyone.herokuapp.com/https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=(echocardiography[Title/Abstract])+AND+(preterm[Title/Abstract])&retmode=json&retmax=20&sort=relevance&field=pubdate';
 
 // Função para buscar artigos na API do PubMed
 async function buscarArtigos() {
   try {
     // Fazer a solicitação de busca
-    const response = await fetch(url, {
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    });
-    const data = await response.json();
+    const response = await axios.get(url);
+    const data = response.data;
 
     // Obter lista de IDs de artigos
     const ids = data.esearchresult.idlist;
 
     // Para cada ID, obter informações do artigo
     const artigos = await Promise.all(ids.map(async (id) => {
-      const res = await fetch(`${proxyUrl}https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=${id}&retmode=json`, {
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest'
-        }
-      });
-      const json = await res.json();
+      const res = await axios.get(`https://cors-anywhere-for-everyone.herokuapp.com/https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=${id}&retmode=json`);
+      const json = res.data;
       return json.result[id];
     }));
 
@@ -37,25 +26,4 @@ async function buscarArtigos() {
     listaArtigos.innerHTML = '';
     artigos.forEach((artigo) => {
       const li = document.createElement('li');
-      const doi = artigo.articleids.filter((id) => id.idtype === 'doi')[0];
-      const pmid = artigo.articleids.filter((id) => id.idtype === 'pubmed')[0];
-      const link = document.createElement('a');
-      link.href = `https://www.ncbi.nlm.nih.gov/pubmed/${pmid.value}`;
-      link.target = '_blank';
-      link.rel = 'noopener noreferrer';
-      link.textContent = artigo.title;
-      li.appendChild(link);
-      const info = document.createElement('span');
-      info.textContent = ` (${new Date(artigo.pubdate).toLocaleDateString()})`;
-      li.appendChild(info);
-      listaArtigos.appendChild(li);
-    });
-  } catch (e) {
-    console.error('Erro ao buscar artigos:', e);
-  }
-}
-
-// Chamar a função buscarArtigos ao carregar a página
-window.onload = buscarArtigos;
-
-
+      const doi = artigo.articleids.filter
