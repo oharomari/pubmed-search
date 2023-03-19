8f5aa802c0e249de527ad71b7676e2e8a6efbc38
oharomari <128266847+oharomari@users.noreply.github.com>
2023-03-18 22:57:49 -0300

    fechando

--- script.js
+++ script.js
@@ -35,4 +35,36 @@ async function buscarArtigosPubmed() {
       return json.result[id];
     }));
 
-   
+    // Ordenar artigos por data de publicação
+    artigos.sort((a, b) => new Date(b.pubdate) - new Date(a.pubdate));
+
+    // Exibir os artigos na página
+    const listaArtigos = document.getElementById('lista-artigos');
+    listaArtigos.innerHTML = '';
+    artigos.forEach((artigo) => {
+      const li = document.createElement('li');
+      const doi = artigo.articleids.filter((id) => id.idtype === 'doi')[0];
+      const pmid = artigo.articleids.filter((id) => id.idtype === 'pubmed')[0];
+      const link = document.createElement('a');
+      link.href = `https://www.ncbi.nlm.nih.gov/pubmed/${pmid.value}`;
+      link.target = '_blank';
+      link.rel = 'noopener noreferrer';
+      link.textContent = artigo.title;
+      li.appendChild(link);
+      const info = document.createElement('span');
+      info.textContent = ` (${new Date(artigo.pubdate).toLocaleDateString()})`;
+      li.appendChild(info);
+      listaArtigos.appendChild(li);
+    });
+  } catch (e) {
+    console.error('Erro ao buscar artigos no PubMed:', e);
+  }
+}
+
+// Função para buscar artigos na API do Google Scholar
+async function buscarArtigosGoogle() {
+  try {
+    const response = await fetch(googleUrl);
+    const data = await response.text();
+    const parser = new DOMParser();
+    const doc =
